2c59aa9e259b98f2e8dc9dea113da1ea79f3c77a
Solanaceae <89823371+Atropa-Solanaceae@users.noreply.github.com>
2024-06-30 00:03:27 -0400

    Delete desktop2srv.sh

--- desktop2srv.sh
+++
@@ -1,6 +0,0 @@
-sudo apt install ubuntu-server
-reboot
-sudo systemctl set-default multi-user.target
-reboot
-sudo apt purge ubuntu-desktop -y && sudo apt autoremove -y && sudo apt autoclean
-reboot
